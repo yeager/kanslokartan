1cc30bd019058ce15a6a178347ce804d39e01096
Daniel Nylander <po@danielnylander.se>
2026-02-21 15:59:53 +0100

    feat: add kanslokartan application

---
+++ src/kanslokartan/main.py
@@ -0,0 +1,242 @@
+"""Känslokartan - Emotion recognition training."""
+import sys
+import os
+import json
+import random
+import gettext
+import locale
+import gi
+gi.require_version('Gtk', '4.0')
+gi.require_version('Adw', '1')
+from gi.repository import Gtk, Adw, Gio, GLib, Gdk
+from kanslokartan import __version__
+from kanslokartan.accessibility import apply_large_text
+
+TEXTDOMAIN = "kanslokartan"
+for p in [os.path.join(os.path.dirname(__file__), "locale"), "/usr/share/locale"]:
+    if os.path.isdir(p):
+        gettext.bindtextdomain(TEXTDOMAIN, p)
+        locale.bindtextdomain(TEXTDOMAIN, p)
+        break
+gettext.textdomain(TEXTDOMAIN)
+_ = gettext.gettext
+
+EMOTIONS = [
+    {"id": 28530, "name": _("Happy"), "emoji": "\U0001f600"},
+    {"id": 28531, "name": _("Sad"), "emoji": "\U0001f622"},
+    {"id": 28529, "name": _("Angry"), "emoji": "\U0001f621"},
+    {"id": 28534, "name": _("Scared"), "emoji": "\U0001f628"},
+    {"id": 28532, "name": _("Surprised"), "emoji": "\U0001f632"},
+    {"id": 28533, "name": _("Disgusted"), "emoji": "\U0001f922"},
+    {"id": 6730, "name": _("Calm"), "emoji": "\U0001f60c"},
+    {"id": 28535, "name": _("Tired"), "emoji": "\U0001f634"},
+    {"id": 6726, "name": _("Worried"), "emoji": "\U0001f61f"},
+    {"id": 28536, "name": _("Proud"), "emoji": "\U0001f60e"},
+]
+
+CONFIG_DIR = os.path.join(GLib.get_user_config_dir(), "kanslokartan")
+RESULTS_FILE = os.path.join(CONFIG_DIR, "results.json")
+
+
+def _load_results():
+    try:
+        with open(RESULTS_FILE) as f:
+            return json.load(f)
+    except (FileNotFoundError, json.JSONDecodeError):
+        return []
+
+
+def _save_results(results):
+    os.makedirs(CONFIG_DIR, exist_ok=True)
+    with open(RESULTS_FILE, "w") as f:
+        json.dump(results[-500:], f, ensure_ascii=False, indent=2)
+
+
+class KansloApp(Adw.Application):
+    def __init__(self):
+        super().__init__(application_id="se.danielnylander.kanslokartan",
+                         flags=Gio.ApplicationFlags.DEFAULT_FLAGS)
+
+    def do_activate(self):
+        apply_large_text()
+        win = self.props.active_window or KansloWindow(application=self)
+        win.present()
+
+    def do_startup(self):
+        Adw.Application.do_startup(self)
+        for name, cb, accel in [
+            ("quit", lambda *_: self.quit(), "<Control>q"),
+            ("about", self._on_about, None),
+            ("shortcuts", self._on_shortcuts, "<Control>slash"),
+            ("export", self._on_export, "<Control>e"),
+        ]:
+            a = Gio.SimpleAction.new(name, None)
+            a.connect("activate", cb)
+            self.add_action(a)
+            if accel:
+                self.set_accels_for_action(f"app.{name}", [accel])
+
+    def _on_about(self, *_):
+        d = Adw.AboutDialog(
+            application_name=_("Emotion Map"), application_icon="kanslokartan",
+            version=__version__, developer_name="Daniel Nylander",
+            website="https://www.autismappar.se",
+            issue_url="https://github.com/yeager/kanslokartan/issues",
+            license_type=Gtk.License.GPL_3_0, developers=["Daniel Nylander"],
+            copyright="\u00a9 2026 Daniel Nylander")
+        d.present(self.props.active_window)
+
+    def _on_shortcuts(self, *_):
+        pass  # TODO
+
+    def _on_export(self, *_):
+        w = self.props.active_window
+        if w:
+            w.do_export()
+
+
+class KansloWindow(Adw.ApplicationWindow):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs, default_width=500, default_height=650,
+                         title=_("Emotion Map"))
+        self.score = 0
+        self.total = 0
+        self.current = None
+        self.results = _load_results()
+        self._build_ui()
+        self._next_emotion()
+
+    def _build_ui(self):
+        box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+        self.set_content(box)
+        header = Adw.HeaderBar()
+        box.append(header)
+
+        menu = Gio.Menu()
+        menu.append(_("Export Results"), "app.export")
+        menu.append(_("Keyboard Shortcuts"), "app.shortcuts")
+        menu.append(_("About Emotion Map"), "app.about")
+        menu.append(_("Quit"), "app.quit")
+        header.pack_end(Gtk.MenuButton(icon_name="open-menu-symbolic", menu_model=menu))
+
+        theme_btn = Gtk.Button(icon_name="weather-clear-night-symbolic",
+                               tooltip_text=_("Toggle dark/light theme"))
+        theme_btn.connect("clicked", self._toggle_theme)
+        header.pack_end(theme_btn)
+
+        self.score_label = Gtk.Label(label=_("Score: 0 / 0"))
+        self.score_label.add_css_class("title-4")
+        self.score_label.set_margin_top(12)
+        box.append(self.score_label)
+
+        self.emoji_label = Gtk.Label()
+        self.emoji_label.set_markup('<span size="120000">\u2753</span>')
+        self.emoji_label.set_margin_top(20)
+        self.emoji_label.set_margin_bottom(20)
+        box.append(self.emoji_label)
+
+        self.prompt_label = Gtk.Label(label=_("What emotion is this?"))
+        self.prompt_label.add_css_class("title-2")
+        self.prompt_label.set_margin_bottom(16)
+        box.append(self.prompt_label)
+
+        self.btn_grid = Gtk.FlowBox(max_children_per_line=2,
+                                     selection_mode=Gtk.SelectionMode.NONE,
+                                     homogeneous=True, row_spacing=8, column_spacing=8)
+        self.btn_grid.set_margin_start(24)
+        self.btn_grid.set_margin_end(24)
+        box.append(self.btn_grid)
+
+        self.feedback_label = Gtk.Label(label="")
+        self.feedback_label.add_css_class("title-3")
+        self.feedback_label.set_margin_top(16)
+        box.append(self.feedback_label)
+
+        self.next_btn = Gtk.Button(label=_("Next"))
+        self.next_btn.add_css_class("suggested-action")
+        self.next_btn.add_css_class("pill")
+        self.next_btn.set_halign(Gtk.Align.CENTER)
+        self.next_btn.set_margin_top(12)
+        self.next_btn.set_margin_bottom(16)
+        self.next_btn.connect("clicked", lambda *_: self._next_emotion())
+        self.next_btn.set_visible(False)
+        box.append(self.next_btn)
+
+        self.status_label = Gtk.Label(label="", xalign=0)
+        self.status_label.add_css_class("dim-label")
+        self.status_label.set_margin_start(12)
+        self.status_label.set_margin_bottom(4)
+        box.append(self.status_label)
+        GLib.timeout_add_seconds(1, self._update_clock)
+        self._update_clock()
+
+    def _next_emotion(self):
+        self.current = random.choice(EMOTIONS)
+        self.emoji_label.set_markup(f'<span size="120000">{self.current["emoji"]}</span>')
+        self.feedback_label.set_label("")
+        self.next_btn.set_visible(False)
+
+        wrong = [e for e in EMOTIONS if e["id"] != self.current["id"]]
+        choices = random.sample(wrong, min(3, len(wrong))) + [self.current]
+        random.shuffle(choices)
+
+        while (child := self.btn_grid.get_first_child()):
+            self.btn_grid.remove(child)
+
+        for em in choices:
+            btn = Gtk.Button(label=em["name"])
+            btn.add_css_class("pill")
+            btn.set_size_request(180, 48)
+            btn.connect("clicked", self._on_answer, em)
+            self.btn_grid.append(btn)
+
+    def _on_answer(self, btn, chosen):
+        self.total += 1
+        correct = chosen["id"] == self.current["id"]
+        if correct:
+            self.score += 1
+            self.feedback_label.set_label(_("Correct! \u2705"))
+        else:
+            self.feedback_label.set_label(_("Not quite. It was: %s") % self.current["name"])
+
+        self.score_label.set_label(_("Score: %d / %d") % (self.score, self.total))
+
+        child = self.btn_grid.get_first_child()
+        while child:
+            inner = child.get_first_child()
+            if inner:
+                inner.set_sensitive(False)
+            child = child.get_next_sibling()
+        self.next_btn.set_visible(True)
+
+        from datetime import datetime
+        self.results.append({"date": datetime.now().isoformat(), "emotion": self.current["name"],
+                              "chosen": chosen["name"], "correct": correct})
+        _save_results(self.results)
+
+    def do_export(self):
+        from kanslokartan.export import export_csv, export_json
+        os.makedirs(CONFIG_DIR, exist_ok=True)
+        ts = GLib.DateTime.new_now_local().format("%Y%m%d_%H%M%S")
+        data = [{"date": r["date"], "details": r["emotion"],
+                 "result": str(r["correct"])} for r in self.results]
+        export_csv(data, os.path.join(CONFIG_DIR, f"export_{ts}.csv"))
+        export_json(data, os.path.join(CONFIG_DIR, f"export_{ts}.json"))
+        self.feedback_label.set_label(_("Exported to %s") % CONFIG_DIR)
+
+    def _toggle_theme(self, *_):
+        mgr = Adw.StyleManager.get_default()
+        mgr.set_color_scheme(
+            Adw.ColorScheme.FORCE_LIGHT if mgr.get_dark() else Adw.ColorScheme.FORCE_DARK)
+
+    def _update_clock(self):
+        self.status_label.set_label(GLib.DateTime.new_now_local().format("%Y-%m-%d %H:%M:%S"))
+        return True
+
+
+def main():
+    app = KansloApp()
+    app.run(sys.argv)
+
+if __name__ == "__main__":
+    main()
